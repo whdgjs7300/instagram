b5c8d530d4d740d435bb8dbc9f358c13e654bc5d
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2024-01-11 20:57:37 +0900

    1/11 로그인 페이지 구현 (next auth)

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,7 +1,31 @@
+import Signin from '@/components/Signin';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth'
+import { getProviders } from 'next-auth/react';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-export default function SignPage() {
+type Props = {
+  searchParams : {
+    callbackUrl : string,
+  }
+};
+
+
+export default async function SignPage({searchParams : {callbackUrl}} : Props) {
+  const session = await getServerSession(authOptions);
+  
+  if(session) {
+    redirect('/');
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+
   return (
-    <div>page</div>
+    <section className='flex justify-center mt-[30%]'>
+      <Signin  providers={providers} callbackUrl={callbackUrl ?? '/'}/>
+    </section>
+    
   )
 }
