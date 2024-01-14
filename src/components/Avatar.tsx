243e693526b43c0c59516cf202ed36c03a9c52ab
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2024-01-14 21:41:26 +0900

    1/14 SideBar 동적 스타일링

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
 
-type Props = {image? : string | null}
+type Props = {
+    image? : string | null
+    size? : 'small' | 'normal',
+    highlight? : boolean,
+}
 
-export default function Avatar({image} : Props) {
+export default function Avatar({
+    image, 
+    size='normal', 
+    highlight=false,
+} : Props) {
     return (
-        <div className='w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300'>
+        <div className={getContainerStyle(size, highlight)}>
             <img 
-            className='rounded-full p-[0.1rem]'
+            className={`bg-white rounded-full p-[0.1rem] ${getImageSizeStyle(size)}`}
             alt='user profile' src={image ?? undefined}
-            referrerPolicy='no-referrer' />
+            referrerPolicy='no-referrer' 
+            />
         </div>
     )
+}
+
+function getContainerStyle(size: string, highlight: boolean): string {
+    const baseStyle = 'rounded-full flex justify-center items-center';
+    const highlightStyle = highlight ? 'bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300' 
+    : '';
+    const sizeStyle = size === 'small' ? 'w-9 h-9' : 'w-[68px] h-[68px]'
+    return `${baseStyle} ${highlightStyle} ${sizeStyle}`;
+} 
+
+function getImageSizeStyle(size:string) : string {
+    return size === 'small' 
+    ? 'w-[34px] h-[34px] p-[0.1rem]' 
+    : 'w-16 h-16 p-[0.2rem]';
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,21 +6,24 @@ type Props = {
     user : User;
 }
 
-export default function SideBar({user : {name, username, image}} : Props) {
+export default function SideBar({ user: { name, username, image } }: Props) {
     return (
         <>
-            <div>
+            <div className='flex item-center'>
             {
-                image && <Avatar image={image} />
+                image && <Avatar image={image} highlight />
             }
-            <p>{username}</p>
-            <p>{name}</p>
+            <div className='ml-4'>
+                <p className='font-bold'>{username}</p>
+                <p className='text-lg text-neutral-500 leading-4'>{name}</p>
             </div>
-            <p>
+            </div>
+
+            <p className='text-sm text-neutral-500 mt-8'>
                 About ' Help ' Press ' API ' Jobs ' Privacy ' Terms ' Location ' 
                 Language
             </p>
-            <p>
+            <p className='font-bold text-sm mt-8 text-neutral-500'>
                 @Copyright INSTANTGRAM from METAL
             </p>
         </>
